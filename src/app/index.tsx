512956b5e14531d3a3e87d355b1bdfb335691d4c
s-yamauchi07 <hand1nhand306@gmail.com>
2025-08-05 18:38:41 +0900

    MarkUp create memo components

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import { Redirect }  from 'expo-router'
 
 const Index = (): React.JSX.Element => {
-  return <Redirect href="/memo/edit" />
+  return <Redirect href="/memo/create" />
 }
 
 export default Index
