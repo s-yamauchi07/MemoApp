9f7585ca093346799329512edb6be27894194b63
s-yamauchi07 <hand1nhand306@gmail.com>
2025-08-05 13:32:02 +0900

    header styling

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,10 +4,10 @@ const Index = (): React.JSX.Element => {
   return (
     <View style={styles.container}>
       {/* ヘッダー */}
-      <View>
-        <View>
-          <Text>Memo App</Text>
-          <Text>ログアウト</Text>
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>Memo App</Text>
+          <Text style={styles.headerRight}>ログアウト</Text>
         </View>
       </View>
 
@@ -55,10 +55,29 @@ const Index = (): React.JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff'
+  },
+  header: {
+    backgroundColor: '#467fd3',
+    height: 104,
+    justifyContent: 'flex-end'
+  },
+  headerInner: {
     alignItems: 'center'
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 22,
+    fontWeight: 'bold',
+    color: '#fff'
+  },
+  headerRight: {
+    position: 'absolute',
+    right: 16,
+    bottom: 16,
+    color: 'rgba(255, 255, 255, 0.7)'
   }
-
 })
 
 export default Index
