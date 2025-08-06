1d60315b41b2452f01375f6301fe8204d3baa160
s-yamauchi07 <hand1nhand306@gmail.com>
2025-08-06 09:39:14 +0900

    Markup Login components

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import { Redirect }  from 'expo-router'
 
 const Index = (): React.JSX.Element => {
-  return <Redirect href="/memo/create" />
+  return <Redirect href="auth/login" />
 }
 
 export default Index
