4e028ec5cc48af1faee2528e1b94851a56042a5b
s-yamauchi07 <hand1nhand306@gmail.com>
2025-08-06 09:53:50 +0900

    Login components styling

--- src/app/auth/login.tsx
+++ src/app/auth/login.tsx
@@ -5,16 +5,16 @@ const Login = (): React.JSX.Element => {
   return(
     <View style={styles.container}>
       <Header />
-      <View>
-        <Text>Login</Text>
-        <TextInput value="Email address" />
-        <TextInput value="Password" />
-        <View>
-          <Text>Submit</Text>
+      <View style={styles.inner}>
+        <Text style={styles.title}>Login</Text>
+        <TextInput style={styles.input} value="Email address" />
+        <TextInput style={styles.input} value="Password" />
+        <View style={styles.button} >
+          <Text style={styles.buttonLabel}>Submit</Text>
         </View>
-        <View>
-          <Text>Not registered?</Text>
-          <Text>Sign up here!</Text>
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Not registered?</Text>
+          <Text style={styles.footerLink}>Sign up here!</Text>
         </View>
       </View>
     </View>
@@ -23,7 +23,54 @@ const Login = (): React.JSX.Element => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: '#f0f4fb'
+  },
+  inner: {
+    paddingVertical: 24,
+    paddingHorizontal: 27
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    lineHeight: 32,
+    marginBlock: 24
+  },
+  input: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    height: 48,
+    padding: 8,
+    fontSize: 16,
+    marginBottom: 16
+  },
+  button: {
+    backgroundColor: '#467fd3',
+    borderRadius: 4,
+    alignSelf: 'flex-start',
+    marginBottom: 24
+  },
+  buttonLabel: {
+    fontSize: 16,
+    lineHeight: 32,
+    color: '#fff',
+    paddingVertical: 8,
+    paddingHorizontal: 24
+  },
+  footer: {
+    flexDirection: 'row'    
+  },
+  footerText: {
+    fontSize: 14,
+    lineHeight: 24,
+    marginRight: 8,
+    color: '#666'
+  },
+  footerLink: {
+    fontSize: 14,
+    lineHeight: 24,
+    color: '#467fd3'
   }
 })
 
