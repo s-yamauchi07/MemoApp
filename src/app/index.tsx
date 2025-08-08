fd478bd38288da64e9589fafcc0a04f0564c4c18
s-yamauchi07 <hand1nhand306@gmail.com>
2025-08-08 11:13:35 +0900

    watch user login status

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,21 @@
-import { Redirect }  from 'expo-router'
+import { Redirect, router }  from 'expo-router'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../config'
+import { useEffect } from 'react'
 
 const Index = (): React.JSX.Element => {
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user !== null) {
+        // ユーザーがログインしている場合、メモ一覧画面にリダイレクト
+        router.replace('/memo/list')
+      } else {
+        // ユーザーがログインしていない場合、サインアップ画面にリダイレクト
+        router.replace('/auth/signup')
+      }
+    })
+  }, [])
+  
   return <Redirect href="auth/signup" />
 }
 
